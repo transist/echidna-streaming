c79bd1717e227c330ceeb9c1ff0dd58bc759a805
Richard Huang <flyerhzm@gmail.com>
2013-02-22 10:58:01 +0800

    subscribe add_user, add_group, add_user_to_group, add_tweet and add_word messages

--- lib/worker.rb
+++ lib/worker.rb
@@ -9,20 +9,37 @@ EM.synchrony do
     Redis.new driver: :synchrony
   end
 
-  $redis.subscribe("tencent_weibo_users", "tencent_weibo_tweets") do |on|
+  # publish add_user '{"id":"user-1","type":"tencent","birth_year":2000,"gender":"f","city":"shanghai"}'
+  # publish add_group '{"id":"group-1","name":"Group 1"}'
+  # publish add_user_to_group '{"group_id":"group-1","user_id":"user-1","user_type":"tencent"}'
+  # publish add_tweet '{"user_id":"user-1","user_type":"tencent","text":"我是中国人","id":"abc","url":"http://t.qq.com/t/abc","timestamp":1361494534}'
+  $redis.subscribe("add_user", "add_group", "add_user_to_group", "add_tweet", "add_word") do |on|
     on.message do |channel, msg|
-      case channel
-      when "tencent_weibo_users"
-        user_attributes = MultiJson.decode(msg)
-        User.new(user_attributes).save
-      when "tencent_weibo_tweets"
-        algorithm = RMMSeg::Algorithm.new(msg)
-        segments = []
-        loop do
-          token = algorithm.next_token
-          break if token.nil?
-          segments << token.text
+      attributes = MultiJson.decode(msg)
+      p attributes
+      begin
+        case channel
+        when "add_user"
+          User.new(attributes).save
+        when "add_group"
+          Group.new(attributes).save
+        when "add_user_to_group"
+          user = User.new("id" => attributes["user_id"], "type" => attributes["user_type"])
+          Group.new("id" => attributes["group_id"]).add_user(user)
+        when "add_tweet"
+          group_id = User.new(attributes).group_id
+          algorithm = RMMSeg::Algorithm.new(attributes['text'])
+          segments = []
+          loop do
+            token = algorithm.next_token
+            break if token.nil?
+            $redis.publish "add_word", MultiJson.encode({"group_id" => group_id, "timestamp" => attributes['timestamp'], "word" => token.text})
+          end
+        when "add_word"
+          Keyword.new(attributes).save
         end
+      #rescue
+        #puts $!.message
       end
     end
   end
