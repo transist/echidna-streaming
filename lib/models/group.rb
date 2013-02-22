5866a45879a627d77ababb31f9a0123bf513056c
Richard Huang <flyerhzm@gmail.com>
2013-02-22 20:31:19 +0800

    add Group#trends to get words trend

--- lib/models/group.rb
+++ lib/models/group.rb
@@ -1,11 +1,21 @@
 # coding: utf-8
+require 'set'
+
 class Group < Base
   def save
     $redis.hmset key, *attributes.slice('name').to_a.flatten
   end
 
-  def keywords(interval, start_timestamp, end_timestamp)
-    $redis.zrangebyscore "groups:#{@attributes['id']}:#{interval}:keywords", start_timestamp, end_timestamp
+  def trends(interval, start_timestamp, end_timestamp)
+    keys = $redis.zrangebyscore "groups:#{@attributes['id']}:#{interval}:keywords", start_timestamp.to_i, end_timestamp.to_i
+    trends = {}
+    keys.each do |key|
+      count = $redis.get(key).to_i
+      _, _, _, timestamp, word = key.split(':')
+      trends[timestamp] ||= {}
+      trends[timestamp][word] = count
+    end
+    trends
   end
 
   def add_user(user)
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -17,19 +17,20 @@ describe Group do
     end
   end
 
-  context "#keywords" do
+  context "#trends" do
     subject { Group.new("id" => "group-1") }
     before do
       Keyword.new({"group_id" => "group-1", "timestamp" => 1361494534, "word" => "中国"}).save
-      Keyword.new({"group_id" => "group-1", "timestamp" => 1361496640, "word" => "中间"}).save
+      Keyword.new({"group_id" => "group-1", "timestamp" => 1361494534, "word" => "中间"}).save
+      Keyword.new({"group_id" => "group-1", "timestamp" => 1361494638, "word" => "中国"}).save
     end
 
-    it "should get all keys when timestamp in the period" do
-      expect(subject.keywords('minute', 1361494500, 1361496600)).to eq ["groups:group-1:minute:1361494500:中国", "groups:group-1:minute:1361496600:中间"]
-      expect(subject.keywords('hour', 1361491200, 1361523600)).to eq ["groups:group-1:hour:1361491200:中国", "groups:group-1:hour:1361494800:中间"]
-      expect(subject.keywords('day', 1361491200, 1361491200)).to eq ["groups:group-1:day:1361491200:中国", "groups:group-1:day:1361491200:中间"]
-      expect(subject.keywords('month', 1359676800, 1359676800)).to eq ["groups:group-1:month:1359676800:中国", "groups:group-1:month:1359676800:中间"]
-      expect(subject.keywords('year', 1356998400, 1356998400)).to eq ["groups:group-1:year:1356998400:中国", "groups:group-1:year:1356998400:中间"]
+    it "should get trends when timestamp in the period" do
+      expect(subject.trends('minute', 1361494500, 1361496600)).to eq("1361494500" => {"中国"=>1, "中间"=>1}, "1361494620" => {"中国"=>1})
+      expect(subject.trends('hour', 1361491200, 1361523600)).to eq("1361491200" => {"中国"=>2, "中间"=>1})
+      expect(subject.trends('day', 1361491200, 1361491200)).to eq("1361491200" => {"中国"=>2, "中间"=>1})
+      expect(subject.trends('month', 1359676800, 1359676800)).to eq("1359676800" => {"中国"=>2, "中间"=>1})
+      expect(subject.trends('year', 1356998400, 1356998400)).to eq("1356998400" => {"中国"=>2, "中间"=>1})
     end
   end
 
