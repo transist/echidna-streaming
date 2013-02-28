90da16ad5c85057c608405a482c7f3d28ef39494
Richard Huang <flyerhzm@gmail.com>
2013-02-28 20:37:20 +0800

    init groups, tiers and cities

--- bin/init_groups.rb
+++ bin/init_groups.rb
@@ -16,8 +16,13 @@ Tier.new("id" => "tier-1", "name" => "Tier 1").save
 %w(北京 上海 广州 深圳 天津 重庆).each do |city|
   City.new("name" => city)["tier_id"] = "tier-1"
 end
+Group.new("id" => "group-1", "name" => "Group 1", "gender" => "female", "start_birth_year" => 1989, "end_birth_year" => 1995, "tier_id" => "tier-1").save
 
 Tier.new("id" => "tier-2", "name" => "Tier 2").save
 %w(南京 武汉 沈阳 西安 成都 杭州 济南 青岛 大连 宁波 苏州 无锡 哈尔滨 长春 厦门 佛山 东莞 合肥 郑州 长沙 福州 石家庄 乌鲁木齐 昆明 兰州 南昌 贵阳 南宁 太原 呼和浩特 常州 唐山 准二线 烟台 泉州 包头 徐州 南通 邯郸 温州).each do |city|
   City.new("name" => city)["tier_id"] = "tier-2"
 end
+Group.new("id" => "group-2", "name" => "Group 2", "gender" => "female", "start_birth_year" => 1989, "end_birth_year" => 1995, "tier_id" => "tier-2").save
+
+Tier.new("id" => "tier-other", "name" => "Other Tier").save
+Group.new("id" => "group-other", "name" => "Other Group", "gender" => "female", "start_birth_year" => 1989, "end_birth_year" => 1995, "tier_id" => "tier-other").save
