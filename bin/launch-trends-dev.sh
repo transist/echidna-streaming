1283800f403f9860b5fc6b7982b00e14019cc47e
Ricky Ng-Adam <rngadam@gmail.com>
2013-02-28 15:40:33 +0800

    test scripts from shell

---
+++ bin/launch-trends-dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DIR=`dirname $0`
+source $HOME/.rvm/scripts/rvm
+. $DIR/config-dev.sh
+echo $ECHIDNA_ENV
+ruby $DIR/../trends.rb -sv
---
+++ bin/test-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=`dirname $0`
+source $DIR/config-dev.sh
+echo "redis namespace: ${ECHIDNA_REDIS_NAMESPACE}"
+redis-cli publish ${ECHIDNA_REDIS_NAMESPACE}:add_user '{"id":"user-1","type":"tencent","birth_year":2000,"gender":"f","city":"shanghai"}'
+redis-cli publish ${ECHIDNA_REDIS_NAMESPACE}:add_group '{"id":"group-1","name":"Group 1"}'
+redis-cli publish ${ECHIDNA_REDIS_NAMESPACE}:add_user_to_group '{"group_id":"group-1","user_id":"user-1","user_type":"tencent"}'
+redis-cli publish ${ECHIDNA_REDIS_NAMESPACE}:add_tweet '{"user_id":"user-1","user_type":"tencent","text":"我是中国人","id":"abc","url":"http://t.qq.com/t/abc","timestamp":"20130222005534"}'
+curl "localhost:${ECHIDNA_STREAMING_PORT}?group_id=group-1&interval=minute&start_timestamp=20130222000000&end_timestamp=20130222013000"
