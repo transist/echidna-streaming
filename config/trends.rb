9f0f54a049f87925d86df529b28e4058b3fe003c
Richard Huang <flyerhzm@gmail.com>
2013-03-02 18:16:51 +0800

    update echidna-ruby

--- config/trends.rb
+++
@@ -1,2 +0,0 @@
-$redis = Redis::Namespace.new($redis_namespace, redis: Redis.new(host: $redis_host, port: $redis_prot, driver: :hiredis))
-$logger.notice("connect to redis: #{$redis_host}:#{$redis_port}/#{$redis_namespace}")
