cdcc4bd073606e5f92d625565be7b39f0263f7cf
Richard Huang <flyerhzm@gmail.com>
2013-03-05 00:03:17 +0800

    set messages to different queue according to group id

--- bin/worker.rb
+++ bin/worker.rb
@@ -30,6 +30,6 @@ while true
       Keyword.new(message["body"].merge("word" => word)).save
     end
     result = Crontab.new("timestamp" => message["body"]["timestamp"], "group_id" => message["body"]["group_id"]).fetch_and_save
-    $redis.lpush "api/messages", MultiJson.encode(result)
+    $redis.lpush "api/messages/#{message['body']['group_id']}/trends", MultiJson.encode(result)
   end
 end
