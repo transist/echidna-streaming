6bd5e25534087fc444bfcfaf250ec06590aacf8b
Richard Huang <flyerhzm@gmail.com>
2013-03-05 00:37:34 +0800

    use redis rpush/blpop instead of lpush/brpop

--- bin/worker.rb
+++ bin/worker.rb
@@ -6,7 +6,7 @@ Bundler.require(:default, ENV['ECHIDNA_ENV'] || "development")
 # redis-cli lpush e:d:streaming/messages '{"type":"add_user_to_group","body":{"group_id":"group-1","user_id":"user-1","user_type":"tencent"}}'
 # redis-cli lpush e:d:streaming/messages '{"type":"add_tweet","body":{"user_id":"user-1","user_type":"tencent","text":"我是中国人","id":"abc","url":"http://t.qq.com/t/abc","timestamp":1361494534}}'
 while true
-  raw_message = $redis.brpop "streaming/messages", 0
+  raw_message = $redis.blpop "streaming/messages", 0
   $logger.notice("streaming receive message: #{raw_message}")
   message = MultiJson.decode raw_message.last
   case message["type"]
@@ -20,7 +20,7 @@ while true
     unless source.exist?
       source.save
       group_id = User.new("id" => message["body"]["user_id"], "type" => message["body"]["user_type"])['group_id']
-      $redis.lpush "dicts/messages", MultiJson.encode(
+      $redis.rpush "dicts/messages", MultiJson.encode(
         type: "segment",
         body: message["body"].slice("text", "timestamp").merge(group_id: group_id, source_id: message["body"]["id"])
       )
@@ -30,6 +30,6 @@ while true
       Keyword.new(message["body"].merge("word" => word)).save
     end
     result = Crontab.new("timestamp" => message["body"]["timestamp"], "group_id" => message["body"]["group_id"]).fetch_and_save
-    $redis.lpush "api/messages/#{message['body']['group_id']}/trends", MultiJson.encode(result)
+    $redis.rpush "api/messages/#{message['body']['group_id']}/trends", MultiJson.encode(result)
   end
 end
