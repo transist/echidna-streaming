a55b476277554a30e8bfa305c20667f69ad3faae
Rainux Luo <rainux@gmail.com>
2013-03-06 12:56:44 +0800

    Escape format string in raw_message before send to syslog

--- bin/worker.rb
+++ bin/worker.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, ENV['ECHIDNA_ENV'] || "development")
 while true
   $redis.incr "streaming/messages/incoming"
   raw_message = $redis.blpop "streaming/messages", 0
-  $logger.notice("streaming receive message: #{raw_message}")
+  $logger.notice("streaming receive message: #{raw_message.replace('%', '%%')}")
   message = MultiJson.decode raw_message.last
   case message["type"]
   when "add_user"
