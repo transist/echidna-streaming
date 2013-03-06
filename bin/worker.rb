e9fe23800dc9f6bf857c4a1d8f8ae61433d81809
Richard Huang <flyerhzm@gmail.com>
2013-03-06 09:34:27 +0800

    statistics working incoming and outgoing messages

--- bin/worker.rb
+++ bin/worker.rb
@@ -6,6 +6,7 @@ Bundler.require(:default, ENV['ECHIDNA_ENV'] || "development")
 # redis-cli lpush e:flyerhzm:d:streaming/messages '{"type":"add_user_to_group","body":{"group_id":"group-1","user_id":"user-1","user_type":"tencent"}}'
 # redis-cli lpush e:flyerhzm:d:streaming/messages '{"type":"add_tweet","body":{"user_id":"user-1","user_type":"tencent","text":"我是中国人","id":"abc","url":"http://t.qq.com/t/abc","timestamp":1361494534}}'
 while true
+  $redis.incr "streaming/messages/incoming"
   raw_message = $redis.blpop "streaming/messages", 0
   $logger.notice("streaming receive message: #{raw_message}")
   message = MultiJson.decode raw_message.last
@@ -33,4 +34,5 @@ while true
     $redis.rpush "api/messages/#{message['body']['group_id']}/trends", MultiJson.encode(result)
     $redis.ltrim "api/messages/#{message['body']['group_id']}/trends", 0, 999
   end
+  $redis.incr "streaming/messages/outgoing"
 end
