92021bc1798381d05b24b18af24f47209e7ce11a
Ricky Ng-Adam <rngadam@gmail.com>
2013-03-26 18:42:25 +0800

    some context for the code

--- lib/models/crontab.rb
+++ lib/models/crontab.rb
@@ -4,7 +4,13 @@ class Crontab < Base
     result = []
     %w(second minute hour day month year).each do |interval|
       last_timestamp = $redis.get "crontabs/last_#{interval}"
+      # gets the string formatted timestamp cutted to the interval
+      # example of hour timestamp: 2013-01-01T01
       current_timestamp = Timestamp.new(@attributes["timestamp"]).send("to_#{interval}")
+      
+      # now we're comparing the last (currently processing) timestamp to the actual
+      # time right now and seeing if we're ready to summarize the results
+      # is the currently processed time period elapsed?
       if current_timestamp != last_timestamp
         result += Group.new("id" => @attributes["group_id"]).trends(interval, last_timestamp.to_i + 1, current_timestamp) if last_timestamp
         $redis.set "crontabs/last_#{interval}", current_timestamp
